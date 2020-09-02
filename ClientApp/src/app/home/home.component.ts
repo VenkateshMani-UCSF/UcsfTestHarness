2a27e8f6d8b4c95c453b267670c0b2219254e6e9
Steve Curtis <steven.curtis@philips.com>
2020-09-02 10:20:59 -0400

    formatting and wipe codes when button clicked

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -3,45 +3,48 @@ import { Router } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: './home.component.html',
 })
 export class HomeComponent {
 
-  authCode = '';
-  launchCode = '';
+    authCode = '';
+    launchCode = '';
 
-  constructor(readonly router: Router, @Inject(DOCUMENT) readonly document: Document) { }
+    constructor(readonly router: Router, @Inject(DOCUMENT) readonly document: Document) {
+    }
 
-  public updateAuth(value: string) {
-    this.authCode = value;
-  }
+    public updateAuth(value: string) {
+        this.authCode = value;
+    }
 
-  public updateLaunch(value: string) {
-    this.launchCode = value;
-  }
+    public updateLaunch(value: string) {
+        this.launchCode = value;
+    }
 
-  get window(): Window { return this.document.defaultView; }
+    get window(): Window {
+        return this.document.defaultView;
+    }
 
-  public goToPatientAppWithAuthCode() {
-    console.log('authCode: ' + this.authCode);
-    const url = 'https://localhost:4501/' + '?code=' + this.authCode + '&state=PatientApp state';
-    this.redirect(url, '_blank');
-  }
+    public goToPatientAppWithAuthCode() {
+        console.log('authCode: ' + this.authCode);
+        const url = 'https://localhost:4501/' + '?code=' + this.authCode + '&state=PatientApp state';
+        this.redirect(url, '_blank');
+    }
 
-  public goToPatientAppWithLaunchCode() {
-    console.log('authCode: ' + this.authCode);
-    const url = 'https://localhost:4501/' + '?launch=' + this.launchCode;
-    this.redirect(url, '_blank');
-  }
+    public goToPatientAppWithLaunchCode() {
+        console.log('authCode: ' + this.authCode);
+        const url = 'https://localhost:4501/' + '?launch=' + this.launchCode;
+        this.redirect(url, '_blank');
+    }
 
-  public redirect(url: string, target = '_blank'): Promise<boolean> {
-    return new Promise<boolean>( (resolve, reject) => {
-      try {
-        resolve(!!this.window.open(url, target));
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
+    public redirect(url: string, target = '_blank'): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            try {
+                resolve(!!this.window.open(url, target));
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
 }
